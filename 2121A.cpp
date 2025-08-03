59a28d92324ffa7c891f446b762aa74a0f9178a6
Sachin-8125 <devinewolf7@gmail.com>
2025-08-03 18:55:55 +0530

    Letter Home

---
+++ 2121A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n, s;
+        cin >> n >> s;
+        vector<int> x(n);
+        for (int i = 0; i < n; i++) {
+            cin >> x[i];
+        }
+        int min_x = *min_element(x.begin(), x.end());
+        int max_x = *max_element(x.begin(), x.end());
+        int answer = (max_x - min_x) + min(abs(s - min_x), abs(s - max_x));
+        cout << answer << endl;
+    }
+    return 0;
+}
