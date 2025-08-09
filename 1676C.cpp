d57478af5b8508f401cd76018fe086974901a2e6
Sachin-8125 <devinewolf7@gmail.com>
2025-08-09 18:15:54 +0530

    Most Similar Words

---
+++ 1676C.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<string> words(n);
+        for (int i = 0; i < n; ++i)
+            cin >> words[i];
+
+        int min_diff = INT_MAX;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                int diff = 0;
+                for (int k = 0; k < m; ++k) {
+                    diff += abs(words[i][k] - words[j][k]);
+                }
+                min_diff = min(min_diff, diff);
+            }
+        }
+        cout << min_diff << endl;
+    }
+    return 0;
+}
