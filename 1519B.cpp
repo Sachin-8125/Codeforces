dfef18e02dbba15649db32bfb539b2c51f2f4c8d
Sachin-8125 <devinewolf7@gmail.com>
2025-08-11 14:26:49 +0530

    The cake is a lie

---
+++ 1519B.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, k;
+        cin >> n >> m >> k;
+        if (k == n * m - 1)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+    return 0;
+}
