fe1392db80cb31e2eb34666d83fa22fd2c5defce
Sachin-8125 <devinewolf7@gmail.com>
2025-09-02 19:39:12 +0530

    Pretty Permutations

---
+++ A_Pretty_Permutations.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t, n;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        if (n % 2 == 0) {
+            for (int i = 1; i <= n; i += 2)
+                cout << i + 1 << " " << i << " ";
+        } else {
+            for (int i = 1; i <= n - 3; i += 2)
+                cout << i + 1 << " " << i << " ";
+            cout << n - 1 << " " << n << " " << n - 2;
+        }
+        cout << '\n';
+    }
+}
